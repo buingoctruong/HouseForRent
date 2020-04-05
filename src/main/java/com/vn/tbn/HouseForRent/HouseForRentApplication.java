91a08f39ebb9dad73ff5affc7647df7507b22d60
Bùi Ngọc Trường <buingoctruong1508@gmail.com>
2020-04-05 11:17:22 +0900

    Initial commit

---
+++ src/main/java/com/vn/tbn/HouseForRent/HouseForRentApplication.java
@@ -0,0 +1,13 @@
+package com.vn.tbn.HouseForRent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseForRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseForRentApplication.class, args);
+	}
+
+}
